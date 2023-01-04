8d7518cc94b9487c5154b709b717a36e0b494b10
MaylconRamirez <maylcon.ramirez@and.gov.co>
2023-01-03 21:50:55 -0500

    Se agrega primer versión del nivel 1

--- src/main/java/com/operation/quasar/fire/coordinates/services/IAlliancePositionService.java
+++ src/main/java/com/operation/quasar/fire/coordinates/services/IAlliancePositionService.java
@@ -1,5 +1,6 @@
 package com.operation.quasar.fire.coordinates.services;
 
 public interface IAlliancePositionService {
-
+	public double[] GetLocation(double[] distances);
+	public String GetMessage(String [][] messages);
 }
