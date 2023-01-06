760571098998201a7ad28f02da94b7deb5111343
MaylconRamirez <maylcon.ramirez@and.gov.co>
2023-01-05 21:46:39 -0500

    Se agrega control de excepciones

---
+++ src/main/java/com/operation/quasar/fire/coordinates/exceptions/ControllerExceptionHandler.java
@@ -0,0 +1,46 @@
+package com.operation.quasar.fire.coordinates.exceptions;
+
+import java.util.Date;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
+
+@ControllerAdvice
+public class ControllerExceptionHandler {
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public ResponseEntity<ErrorMessage> resourceNotFoundException(IllegalArgumentException ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.NOT_FOUND.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+    
+    return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
+  }
+  
+  @ExceptionHandler(ArrayIndexOutOfBoundsException.class)
+  public ResponseEntity<ErrorMessage> arrayIndexOutOfBoundsException(ArrayIndexOutOfBoundsException ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.NOT_FOUND.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+    
+    return new ResponseEntity<ErrorMessage>(message, HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(Exception.class)
+  public ResponseEntity<ErrorMessage> globalExceptionHandler(Exception ex, WebRequest request) {
+    ErrorMessage message = new ErrorMessage(
+        HttpStatus.INTERNAL_SERVER_ERROR.value(),
+        new Date(),
+        ex.getMessage(),
+        request.getDescription(false));
+    
+    return new ResponseEntity<ErrorMessage>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
---
+++ src/main/java/com/operation/quasar/fire/coordinates/exceptions/ErrorMessage.java
@@ -0,0 +1,19 @@
+package com.operation.quasar.fire.coordinates.exceptions;
+
+import java.util.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorMessage {
+	  private int statusCode;
+	  private Date timestamp;
+	  private String message;
+	  private String description;
+}
